b7adaf02af31f7fe92a1da450ebfb2489f9965da
Maxens Telle <tmax.2017.03@gmail.com>
2023-04-17 10:43:52 +0200

    add React

--- blackjack/blackjack.php
+++ blackjack/blackjack.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
+    <!-- Meta -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Script -->
+    <script crossorigin src="https://unpkg.com/react@16/umd/react.development.js"></script>
+    <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
+    <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+    <!-- CSS -->
     <link rel="stylesheet" href="blackjack.css">
+    <!-- Autre -->
     <title>BlackJack</title>
 </head>
 <body>
